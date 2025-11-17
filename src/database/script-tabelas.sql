db46e8c45f3615103f3688f659f8abb772e7e083
rootdeivid <deivid.oliveira@sptech.school>
2025-11-17 12:15:44 -0300

    validacao de cadastro, login e ambiente

--- src/database/script-tabelas.sql
+++ src/database/script-tabelas.sql
@@ -6,57 +6,32 @@
 comandos para mysql server
 */
 
-CREATE DATABASE aquatech;
+create database ffxiv;
 
-USE aquatech;
+use ffxiv;
 
-CREATE TABLE empresa (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	razao_social VARCHAR(50),
-	cnpj CHAR(14),
-	codigo_ativacao VARCHAR(50)
+create table usuario (
+id INT PRIMARY KEY auto_increment, 
+nome VARCHAR(45),
+apelido VARCHAR(45),
+raca VARCHAR(10),
+email VARCHAR(50),
+senha VARCHAR(20),
+classe VARCHAR(10),
+dataCenter VARCHAR(30),
+fkPersonagens INT,
+CONSTRAINT fkPersonagensUsuario FOREIGN KEY (fkPersonagens) 
+REFERENCES usuario(id));
+
+create table classes (
+id INT PRIMARY KEY auto_increment, 
+nome VARCHAR(20),
+tipo VARCHAR(10))auto_increment=100;
+
+create table mural (
+id INT PRIMARY KEY auto_increment,
+titulo VARCHAR(50),
+publicacao VARCHAR(500),
+descricao VARCHAR(50),
+dtPublicacao DATETIME
 );
-
-CREATE TABLE usuario (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(50),
-	email VARCHAR(50),
-	senha VARCHAR(50),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
-);
-
-CREATE TABLE aviso (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	titulo VARCHAR(100),
-	descricao VARCHAR(150),
-	fk_usuario INT,
-	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
-);
-
-create table aquario (
-/* em nossa regra de negócio, um aquario tem apenas um sensor */
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	descricao VARCHAR(300),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
-);
-
-/* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
-
-create table medida (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	dht11_umidade DECIMAL,
-	dht11_temperatura DECIMAL,
-	luminosidade DECIMAL,
-	lm35_temperatura DECIMAL,
-	chave TINYINT,
-	momento DATETIME,
-	fk_aquario INT,
-	FOREIGN KEY (fk_aquario) REFERENCES aquario(id)
-);
-
-insert into empresa (razao_social, codigo_ativacao) values ('Empresa 1', 'ED145B');
-insert into empresa (razao_social, codigo_ativacao) values ('Empresa 2', 'A1B2C3');
-insert into aquario (descricao, fk_empresa) values ('Aquário de Estrela-do-mar', 1);
-insert into aquario (descricao, fk_empresa) values ('Aquário de Peixe-dourado', 2);
