80fe0f8f0d16238f7c5cc83f2d4d1b661ce83762
rootdeivid <deivid.oliveira@sptech.school>
2025-11-26 18:19:39 -0300

    orgulho da aula de algoritmo e matematica

--- src/models/classeModel.js
+++ src/models/classeModel.js
@@ -1,7 +1,7 @@
 var database = require("../database/config");
 
 function buscarPorId(idUsuario) {
-  var instrucaoSql = `SELECT u.id, c.forca, c.destreza, c.vitalidade, c.destreza, c.inteligencia FROM classes c JOIN usuario u ON u.fk_classe = c.id 
+  var instrucaoSql = `SELECT u.id, c.nome, c.forca, c.destreza, c.vitalidade, c.destreza, c.inteligencia FROM classes c JOIN usuario u ON u.fk_classe = c.id 
   WHERE c.nome = u.classe AND u.id ='${idUsuario}'`;
 
   console.log(`Recebendo ID do Usuario no Models ${idUsuario}`);
